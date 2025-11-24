2baf1bc72e9dc70a2f5306ed6e92e484a82fb96e
tebarek94 <tebarekabas4@gmail.com>
2025-11-24 10:41:57 +0300

    add function in js

---
+++ phase one/Javascripts/code/Operators.js
@@ -0,0 +1,32 @@
+// Arithmetic Operators:
+
+let num1 = 13;
+
+let num2 = 11;
+
+//  addtion
+
+const result = num1 + num2;
+console.log(num1 + num2);
+console.log(result);
+
+// Subtraction)
+console.log(num1 - num2);
+
+// * (Multiplication)
+console.log(num1 * num2);
+
+// / (Division)
+console.log(num1 / num2);
+
+// % (Remainder/Modulo)
+console.log(num1 % num2);
+
+// ** (Exponentiation)
+console.log(num1 ** num2);
+
+// ++ (Increment)
+console.log(++num2);
+
+// -- (Decrement)
+console.log(--num2);
---
+++ phase one/Javascripts/code/function/function.js
@@ -0,0 +1,20 @@
+// // named function
+// function addTwoNumers(num1, num2) {
+//   return num1 + num2;
+// }
+
+// // calling the functrion
+// console.log(addTwoNumers(5, 10));
+
+// arrow function
+// const addTwoNumers = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(addTwoNumers(2, 4));
+
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
+
+// console.log(add(56, 54));
